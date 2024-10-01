1a3ca67241b4f1964ae00c961ff7e39c2bc8bf9b
Gifftybabe <ulokangozi@gmail.com>
2024-10-01 03:35:01 +0100

    index.tsx file update

---
+++ utils/Appkits/ux_heaven/config/index.tsx
@@ -0,0 +1,31 @@
+
+// config/index.tsx
+
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+
+import { cookieStorage, createStorage } from 'wagmi'
+import { mainnet, sepolia } from 'wagmi/chains'
+
+// Your Reown Cloud project ID
+export const projectId = 'e957ca013ed1987ebf1cf5fbe18abbb6'
+
+// Create a metadata object
+const metadata = {
+  name: 'UX-Heaven',
+  description: 'AppKit Example',
+  url: 'https://reown.com/appkit', // origin must match your domain & subdomain
+  icons: ['https://assets.reown.com/reown-profile-pic.png']
+}
+
+// Create wagmiConfig
+const chains = [mainnet, sepolia] as const
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage
+  }),
+  ...wagmiOptions // Optional - Override createConfig parameters
+})
