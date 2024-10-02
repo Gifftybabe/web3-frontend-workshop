d24282ca751fe3146b687e0fc65c7e38b0e1e109
Gifftybabe <ulokangozi@gmail.com>
2024-10-02 11:03:48 +0100

    frontend-ui

---
+++ examples/complete-dapp/greeting/client/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="container">
+  <div class="container text-center">
+  <div class="row">
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>
+</div>
+  );
+}
+
+export default App;
