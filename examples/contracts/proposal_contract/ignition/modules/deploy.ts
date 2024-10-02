238dd1ee219838a93230c54b895c113ef32e9e04
Gifftybabe <ulokangozi@gmail.com>
2024-10-02 18:04:39 +0100

    deployment update

---
+++ examples/contracts/proposal_contract/ignition/modules/deploy.ts
@@ -0,0 +1,11 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const ProposalModule = buildModule("ProposalModule", (m) => {
+
+  const proposal = m.contract("ProposalContract");
+
+
+  return { proposal };
+});
+
+export default ProposalModule;
