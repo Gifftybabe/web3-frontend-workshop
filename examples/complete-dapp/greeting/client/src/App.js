194feb1acdbdfc22f637a4555dceba9e83b603ba
Gifftybabe <ulokangozi@gmail.com>
2024-10-05 06:11:56 +0100

    handleChange and submit logic

--- examples/complete-dapp/greeting/client/src/App.js
+++ examples/complete-dapp/greeting/client/src/App.js
@@ -1,28 +1,55 @@
-import './App.css';
+import { useState, useEffect } from 'react';
 import { ethers } from "ethers";
 
+import './App.css';
+
 function App() {
+  const [greet, setGreet] = useState('')
+  const [greetingValue, setGreetingValue] = useState('')
+  const [depositValue, setDepositValue] = useState('')
+
+
+  const handleDepositChange = (e) => {
+    setDepositValue(e.target.value)
+  }
+
+  const handleGreetingChange = (e) => {
+    setGreetingValue(e.target.value)
+  }
+
+  const handleDepositSubmit = (e) => {
+    e.preventDefault()
+    console.log(depositValue);
+    
+  }
+
+  const handleGreetingSubmit = (e) => {
+    e.preventDefault()
+    console.log(greetingValue);
+    
+  }
+
   return (
-    <div class="container">
-  <div class="container text-center">
-  <div class="row mt-5">
-    <div class="col">
+    <div className="container">
+  <div className="container text-center">
+  <div className="row mt-5">
+    <div className="col">
        <h3>Greeting</h3>
         <p>Contract Balance: 0</p>
     </div>
-    <div class="col">
-      <form>
-      <div class="mb-3">
-        <input type="number" class="form-control" placeholder="0" value="test"/>
-        <button type="submit" class="btn btn-success mt-2">Deposit</button>
+    <div className="col">
+      <form onSubmit={handleDepositSubmit}>
+      <div className="mb-3">
+        <input type="number" className="form-control" placeholder="0" onChange={handleDepositChange} value={depositValue}/>
+        <button type="submit" className="btn btn-success mt-2">Deposit</button>
 
       </div>
     </form>
 
-    <form class="mt-5">
-      <div class="mb-3">
-        <input type="text" class="form-control" value="test"/>
-        <button type="submit" class="btn btn-dark mt-2">Change</button>
+    <form className="mt-5" onSubmit={handleGreetingSubmit}>
+      <div className="mb-3">
+        <input type="text" className="form-control" onChange={handleGreetingChange} value={greetingValue}/>
+        <button type="submit" className="btn btn-dark mt-2">Change</button>
 
       </div>
     </form>
